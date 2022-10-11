7d74e23d89e9e0b38a085a2b4b323f1ac5e8af8b
Yashi Gupta <yashig406@gmail.coM>
2022-10-11 23:30:26 +0530

    Modified Min.java and added more lines in readme

--- OneDrive/Documents/1_Hacktoberfest/Main.java
+++ OneDrive/Documents/1_Hacktoberfest/Main.java
@@ -3,5 +3,6 @@ public class Main
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("After Python, I start learning Java as well");
+		System.out.println("Hope, this is the super cool beginning for open source contribution");
 	}
 }
--- OneDrive/Documents/1_Hacktoberfest/readme.md
+++ OneDrive/Documents/1_Hacktoberfest/readme.md
@@ -1,3 +1,4 @@
 # This is my readme.md file
 
 I have start learning Java and this is the first code of Hello Words in Java.
+I have made some changes in it.
